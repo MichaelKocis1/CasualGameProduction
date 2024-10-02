f1996aa15b10766569e3b160fa839dcdd9a0b78a
EricZUCF <ericzhoust18@gmail.com>
2024-10-02 18:25:11 -0400

    Added level select menu

--- CasualGameProduction/Assets/Scripts/MainMenu.cs
+++ CasualGameProduction/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public void PlayLevelOne ()
     {
         SceneManager.LoadScene("SampleScene");
     }
